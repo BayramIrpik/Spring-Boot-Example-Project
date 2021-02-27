2161e9102de6d01e4ba805a43a7a7b29c9f94685
bayram <bayramirpik@gmail.com>
2021-02-27 12:50:30 +0300

    Added missing method for language error

--- springboot/src/main/java/com/example/springboot/config/WebMvcConfig.java
+++ springboot/src/main/java/com/example/springboot/config/WebMvcConfig.java
@@ -2,8 +2,10 @@ package com.example.springboot.config;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -30,5 +32,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+    
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+          = new ReloadableResourceBundleMessageSource();
+        
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
