ef3add94de48316dfed6905f520ccd214f28f15d
bayram <bayramirpik@gmail.com>
2021-03-09 20:42:50 +0300

    added 403, 404 and 500 error handling

--- springboot/src/main/java/com/example/springboot/controller/HomeController.java
+++ springboot/src/main/java/com/example/springboot/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.example.springboot.controller;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -16,7 +19,7 @@ import com.example.springboot.detail.CustomUserDetails;
 import com.example.springboot.entity.User;
 
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController {
 
 	@GetMapping("")
 	public String viewHomePage() {
@@ -35,27 +38,53 @@ public class HomeController {
 		return "signup_form";
 	}
 
-	@GetMapping("/403")
-	public String accessDenied() {
-		return "403";
-	}
-	
 	@GetMapping("/useroperations")
 	public String useroperations(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-		
-		
+
 		model.addAttribute("username", userDetails.getFullName());
 		return "user_operations";
 	}
-	
-	@RequestMapping(value="/logout", method=RequestMethod.GET)  
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {  
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();  
-        if (auth != null){      
-           new SecurityContextLogoutHandler().logout(request, response, auth);  
-        }  
-         return "redirect:/";  
-     }  
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/";
+	}
+
+	@GetMapping("/error")
+	public String handleError(HttpServletRequest request) {
+		String errorPage = "error"; // default
+
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				// handle HTTP 404 Not Found error
+				errorPage = "error/404";
+
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				// handle HTTP 403 Forbidden error
+				errorPage = "error/403";
+
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				// handle HTTP 500 Internal Server error
+				errorPage = "error/500";
+
+			}
+		}
+
+		return errorPage;
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
 }
